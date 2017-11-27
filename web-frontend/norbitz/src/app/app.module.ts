f614175f7c8003e82e01d8e3e6083678a119f9af
Jacob Mason <jacob@jacobmason.net>
2017-11-27 17:35:18 -0600

    Add hurts base path and stub out carnivore and scandals
    Signed-off-by: Jacob Mason <jacob@jacobmason.net>

--- web-frontend/norbitz/src/app/app.module.ts
+++ web-frontend/norbitz/src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material';
 import { MatInputModule } from '@angular/material';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 
 import { ApiModule as NorbitzModule } from '../apis/norbitz';
 import { ApiModule as AirdndModule, BASE_PATH as AirdndBasePath } from '../apis/airdnd';
-import { ApiModule as CarnivoreModule } from '../apis/carnivore-cruise-lines';
-import { ApiModule as HurtsModule } from '../apis/hurts-car-rental';
-import { ApiModule as ScandalsModule } from '../apis/scandals';
+import { ApiModule as CarnivoreModule, BASE_PATH as CarnivoreBasePath } from '../apis/carnivore-cruise-lines';
+import { ApiModule as HurtsModule, BASE_PATH as HurtsBasePath } from '../apis/hurts-car-rental';
+import { ApiModule as ScandalsModule, BASE_PATH as ScandalsBasePath } from '../apis/scandals';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -23,6 +23,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { LodgingComponent } from './lodging/lodging.component';
+
 
 
 @NgModule({
@@ -54,6 +55,9 @@ import { LodgingComponent } from './lodging/lodging.component';
   providers: [
     UserService, 
     { provide: AirdndBasePath, useValue: "http://35.193.67.106/AirDND/AirDND/1.0.1"},
+    { provide: CarnivoreBasePath, useValue: ""},
+    { provide: HurtsBasePath, useValue: "http://softwarebois.com"},
+    { provide: ScandalsBasePath, useValue: ""},
   ],
   bootstrap: [AppComponent]
 })
