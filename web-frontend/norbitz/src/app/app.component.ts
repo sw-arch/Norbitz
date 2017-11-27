1f984b023bbfb054e0013603ada1b51c40d88236
BlakeHolifield <blakeaholifield@gmail.com>
2017-11-27 12:46:34 -0600

    Datepicker

--- web-frontend/norbitz/src/app/app.component.ts
+++ web-frontend/norbitz/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-
-import { LoginComponent } from './login/login.component';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +7,7 @@ import { LoginComponent } from './login/login.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  date = new FormControl(new Date());
+  serializedDate = new FormControl((new Date()).toISOString());
+
 }
--- web-frontend/norbitz/src/app/app.module.ts
+++ web-frontend/norbitz/src/app/app.module.ts
@@ -5,16 +5,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
-
+import { MatFormFieldModule } from '@angular/material';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
