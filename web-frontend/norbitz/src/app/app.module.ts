f913e6061a62c855da0e388eac64d75ef25bb3d8
Blake <blake@Magnus.local>
2017-11-27 11:24:22 -0600

    Adding router module

--- web-frontend/norbitz/src/app/app.module.ts
+++ web-frontend/norbitz/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -10,9 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
