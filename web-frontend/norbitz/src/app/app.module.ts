9cd80d731daa7d2464503223cebaa6f02c4bac53
Charles Boyd <charlesboyd@users.noreply.github.com>
2017-11-27 11:51:15 -0600

    Added login component

--- web-frontend/norbitz/src/app/app.module.ts
+++ web-frontend/norbitz/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
