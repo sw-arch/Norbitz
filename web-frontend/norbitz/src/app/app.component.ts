18d652bac5f540649886cedb273b5b2cc5ba2abc
Jacob Mason <jacob@jacobmason.net>
2017-11-27 17:39:42 -0600

    Clean up root app component and template
    Signed-off-by: Jacob Mason <jacob@jacobmason.net>

--- web-frontend/norbitz/src/app/app.component.ts
+++ web-frontend/norbitz/src/app/app.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-import { UserService } from './user/user.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor() { }
-
-}
+export class AppComponent { }
