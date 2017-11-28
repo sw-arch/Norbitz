682fe99bf2ca80eced4c3fa0c03e0cc7df55b5e9
BlakeHolifield <blakeaholifield@gmail.com>
2017-11-27 23:47:05 -0600

    Cruises maybe working, blocked by CORS

--- web-frontend/norbitz/src/app/cruises/cruises.component.ts
+++ web-frontend/norbitz/src/app/cruises/cruises.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
+import { DevelopersService, CruiseItem, HistoryItem, AdminsService } from '../../apis/carnivore-cruise-lines';
+import { MatTableDataSource, MatTable } from '@angular/material';
 
 @Component({
   selector: 'app-cruises',
@@ -7,9 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CruisesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private cruises: DevelopersService,
+    private admins: AdminsService,
+  ) { }
+
+  isLoading = true;
+  displayedColumns = ['select','available','cost','name','duration','port',
+                      'roomCapacity', 'roomID', 'depart', 'return'];
+  cruiseDataSource;
+  cruiseItems;
+  selectedCruise = null;
+
+  @Input()
+  city: string = "Atlanta";
+  state: string = "GA";
+  num: number = 1;
+
+
+  @Output() formDone = new EventEmitter();
 
   ngOnInit() {
+    this.cruises.searchForLocation(this.state, this.city, this.num).subscribe(
+      (value: any)=>{
+        //Success
+        console.log("Carnivore search success for "+ this.city + ', ' + this.state);
+        console.log(value);
+        this.cruiseItems = value.cruises;
+        this.cruiseDataSource = new MatTableDataSource<CruiseItem>(this.cruiseItems);
+        this.isLoading = false;
+      },
+      (error)=>{
+        //Error
+        console.log("Carnivore search success for "+ this.city + ', ' + this.state);
+        console.log(error)
+      }
+    );
+  }
+
+  selectCruise(cruiseId){
+    if(cruiseId == this.selectedCruise){
+      this.selectedCruise = null;
+      this.formDone.emit(false);
+    }else{
+      console.log("User selected vehicle " + cruiseId );
+      this.selectedCruise = cruiseId;
+      this.formDone.emit(true);
+    }
+  }
+  buttonStyle(Id){
+    return Id == this.selectedCruise? "bselected" : "bregular";
   }
 
 }
