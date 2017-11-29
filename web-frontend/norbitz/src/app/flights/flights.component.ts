398539c2adb7a97cb75749e3393d95a7738e04a6
Jacob Mason <jacob@jacobmason.net>
2017-11-29 15:51:14 -0600

    Flights WIP
    Signed-off-by: Jacob Mason <jacob@jacobmason.net>

--- web-frontend/norbitz/src/app/flights/flights.component.ts
+++ web-frontend/norbitz/src/app/flights/flights.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { MatTableDataSource, MatTable } from '@angular/material';
+import { PendingorderService } from '../pendingorder/pendingorder.service'
+import { FlightsService, Path } from '../../apis/delter-airlines'
 
 @Component({
   selector: 'app-flights',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flights.component.css']
 })
 export class FlightsComponent implements OnInit {
+  isLoading: boolean = true;
+  delterPaths: Array<Path>;
+  delterDataSource: MatTableDataSource<Path>;
+  displayedColumns = ['select', 'name'];
+  selectedPath: Path;
 
-  constructor() { }
+  constructor(
+    private delter: FlightsService,
+    private pending: PendingorderService,   
+  ) { }
+
+  @Input()
+  origin: string = "Starkville, MS";
+
+  @Input()
+  destination: string = "Huntsville, AL";
+
+  @Input()
+  fromDate: string = "2017-12-01T00:00:00";
+
+  @Input()
+  toDate: string = "2017-12-02T00:00:00";
+
+  @Output()
+  selectionEvent = new EventEmitter();
 
   ngOnInit() {
+    this.delter.fromLocationToLocationStartDateEndDate(this.origin, this.destination, this.fromDate, this.toDate)
+    .subscribe(/*Waiting on https://github.com/henryjr1/DelterAirlinesTeam3/issues/4 */);
   }
-
 }
