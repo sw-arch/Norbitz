1befd2a56c44e243c592d46181ac4270e2b8c494
Jacob Mason <jacob@jacobmason.net>
2017-11-30 00:17:30 -0600

    Update push script to instantly deploy web-fronted image
    Signed-off-by: Jacob Mason <jacob@jacobmason.net>

--- web-frontend/dockerpush.sh
+++ web-frontend/dockerpush.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 ./dockerbuild.sh
 gcloud docker -- push gcr.io/norbitz-185522/web-frontend
+gcloud container clusters get-credentials web-frontend --zone us-central1-a --project norbitz-185522
+kubectl delete `kubectl get pods -o name | grep web-frontend`
