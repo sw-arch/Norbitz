6b675cb65d08dd0a7bd3abedd57785e0968d0588
Jacob Mason <jacob@jacobmason.net>
2017-11-29 21:16:04 -0600

    Remove unnecessary import
    Signed-off-by: Jacob Mason <jacob@jacobmason.net>

--- web-frontend/norbitz/src/app/fun/fun.component.ts
+++ web-frontend/norbitz/src/app/fun/fun.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
-import { MatTableDataSource, MatTable } from '@angular/material';
+import { MatTableDataSource } from '@angular/material';
 import { PendingorderService } from '../pendingorder/pendingorder.service'
 import { DefaultService as ScandalsService, Activity } from '../../apis/scandals';
 
