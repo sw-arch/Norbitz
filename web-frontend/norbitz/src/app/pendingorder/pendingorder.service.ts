85cee5c1a310a01cfe98637bf98e25d099c9553c
Charles Boyd <charlesboyd@users.noreply.github.com>
2017-12-01 00:38:09 -0600

    Working to add cars orders

--- web-frontend/norbitz/src/app/pendingorder/pendingorder.service.ts
+++ web-frontend/norbitz/src/app/pendingorder/pendingorder.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Order, OrderStatus } from './order'
 import { DevelopersService as CruiseDevloperService, CruiseItem } from '../../apis/carnivore-cruise-lines';
+import { UserService as HurtsService, Vehicle, SpecialEquipment } from '../../apis/hurts-car-rental';
 
 @Injectable()
 export class PendingorderService {
 
   constructor(
     private cruises:CruiseDevloperService,
+    private cars:HurtsService,
   ) { }
 
   order: Order = new Order();
@@ -22,18 +24,20 @@ export class PendingorderService {
   submitOrderToThirdPartyServers() {
     console.log("Submiting order to 3rd party servers...");
     this.order.orderPlaced = true;
-    this.order.transCarComplete = OrderStatus.sent;
-    this.order.extraCarComplete = OrderStatus.sent;
-    this.order.funComplete =  OrderStatus.sent;
-    this.order.homestayComplete = OrderStatus.sent;  
-    this.order.cruiseComplete = OrderStatus.sent;
     
-    //TODO: Call service functions
-    this.orderCruise();
+    if(this.order.selectedCruiseId){
+      this.orderCruise();
+    }
+    if(this.order.transCarId){
+      this.orderCar(true);
+    }
+    if(this.order.extraCarId){
+      this.orderCar(false);
+    }
   }
 
   orderCruise(){
-    //this.order.cruiseComplete = OrderStatus.success;    
+    this.order.cruiseComplete = OrderStatus.sent;    
     this.cruises.selectItem(this.order.selectedCruiseId).subscribe(
       (value) => {
         //"Success"
@@ -49,6 +53,37 @@ export class PendingorderService {
         //Error
         console.log("Carnivore order error for "+ this.order.selectedCruiseId);
         this.order.cruiseComplete = OrderStatus.error;        
+      }
+    );
+  }
+
+  orderCar(trans:boolean){
+    if(trans){
+      this.order.transCarComplete = OrderStatus.pending;
+    } else {
+      this.order.extraCarComplete = OrderStatus.pending;
+    } 
+    let vid = trans ? this.order.transCarId : this.order.extraCarId;
+    this.cars.purchaseVehicle(vid, this.order.startDate, this.order.endDate).subscribe(
+      (value) => {
+        //"Success"
+        console.log("Car order success for "+ vid); 
+        console.log(value);
+        if(trans){
+          this.order.transCarComplete = OrderStatus.success;
+        } else {
+          this.order.extraCarComplete = OrderStatus.success;
+        }     
+      },
+      (error) => {
+        //Error
+        console.log("Car order failure for "+ vid);
+        console.log(error);
+        if(trans){
+          this.order.transCarComplete = OrderStatus.error;
+        } else {
+          this.order.extraCarComplete = OrderStatus.error;
+        }        
       }
     );
   }
